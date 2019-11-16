159cd52d3bd8c2aa4079e87d8c64cf4e1d8bcdde
Van Edelman <vanedelman@gmail.com>
2019-11-15 20:58:04 -0500

    added ObjectManager script for storing particle objects and instantiating them

---
+++ Atom Game/Assets/Scripts/Attraction.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attraction : MonoBehaviour
+{
+    //rigid bodies of objects with this script attatched
+    public Rigidbody2D rigidBody;
+
+    private void FixedUpdate()
+    {
+        //AttractAllOfAttraction();
+        
+    }
+
+    //attract another object to this object using newton's law of gravitation
+    private void Attract(Attraction objAttracting)
+    {
+        Rigidbody2D bodyToAttract = objAttracting.rigidBody;
+
+        Vector3 direction = rigidBody.position - bodyToAttract.position;
+        float distance = direction.magnitude;
+
+        float forceMagnitude = (rigidBody.mass * bodyToAttract.mass) / Mathf.Pow(distance, 2);
+        Vector3 gravitationalForce = direction.normalized * forceMagnitude;
+        bodyToAttract.AddForce(gravitationalForce);
+    }
+
+    //attract all objects with this script to this object
+    public void AttractAllOfAttraction()
+    {
+        Attraction[] attractions = FindObjectsOfType<Attraction>();
+        foreach (Attraction attraction in attractions)
+        {
+            if (gameObject.name != attraction.name)
+            {
+                Attract(attraction);
+            }
+        }
+    }
+
+    public void AttractProtons()
+    {
+
+    }
+}
---
+++ Atom Game/Assets/Scripts/ObjectManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum particle { proton, neutron, electron}
+
+public class ObjectManager : MonoBehaviour
+{
+    public GameObject protonPrefab;
+    public GameObject neutronPrefab;
+    public GameObject electronPrefab;
+
+    //data structures to hold all particle objects in the current scene
+    public List<GameObject> protons;
+    public List<GameObject> neutrons;
+    public List<GameObject> electrons;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        protons = new List<GameObject>();
+        neutrons = new List<GameObject>();
+        electrons = new List<GameObject>();
+
+        //test instantiation (comment me out)
+        InstantiateObject(particle.proton, new Vector3(0, 0, 0));
+        InstantiateObject(particle.electron, new Vector3(3, 3, 0));
+        InstantiateObject(particle.neutron, new Vector3(-3, -3, 0));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// instantiates a particle
+    /// </summary>
+    /// <param name="particle">type of particle to instantiate</param>
+    /// <param name="position">position in world space to instantiate prefab at</param>
+    public void InstantiateObject(particle particle, Vector3 position)
+    {
+        switch (particle)
+        {
+            case particle.proton:
+                protons.Add(Instantiate(protonPrefab, position, Quaternion.identity));
+                break;
+            case particle.neutron:
+                neutrons.Add(Instantiate(neutronPrefab, position, Quaternion.identity));
+                break;
+            case particle.electron:
+                neutrons.Add(Instantiate(electronPrefab, position, Quaternion.identity));
+                break;
+        }
+    }
+}
