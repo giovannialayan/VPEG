3474ef2fcda3c10f5cbba97af397f953db1efec6
giovannialayan <giovannialayan@gmail.com>
2019-11-16 15:32:22 -0500

    added a win condition for making hydrogen

--- Atom Game/Assets/Scripts/WinChecker.cs
+++ Atom Game/Assets/Scripts/WinChecker.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WinChecker : MonoBehaviour
 {
-    ObjectManager objectManager;
+    public ObjectManager objectManager;
+    public Text winText;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +17,14 @@ public class WinChecker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        foreach(Atom atom in objectManager.atoms)
+        {
+            if (atom.neutrons == 1 && atom.protons == 1 && atom.electrons == 1)
+            {
+                winText.text = "nice job";
+            }
+        }
+
         
     }
 }
