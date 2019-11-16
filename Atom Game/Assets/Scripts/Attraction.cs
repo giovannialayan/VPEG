a28d1657f04096eeba28a948c26f18d67b7f41cd
giovannialayan <giovannialayan@gmail.com>
2019-11-15 19:50:36 -0500

    created attraction script

---
+++ Atom Game/Assets/Scripts/Attraction.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attraction : MonoBehaviour
+{
+    //rigid bodies of objects with this script attatched
+    public Rigidbody2D rigidBody;
+
+    private void FixedUpdate()
+    {
+        //attract all objects with this script to this object
+        Attraction[] attractions = FindObjectsOfType<Attraction>();
+        foreach(Attraction attraction in attractions)
+        {
+            Attract(attraction);
+        }
+    }
+
+    //attract another object to this object using newton's law of gravitation
+    private void Attract(Attraction objAttracting)
+    {
+        Rigidbody2D bodyToAttract = objAttracting.rigidBody;
+
+        Vector3 direction = rigidBody.position - bodyToAttract.position;
+        float distance = direction.magnitude;
+
+        if (distance > 1)
+        {
+            float forceMagnitude = (rigidBody.mass * bodyToAttract.mass) / Mathf.Pow(distance, 2);
+            Vector3 gravitationalForce = direction.normalized * forceMagnitude;
+            bodyToAttract.AddForce(gravitationalForce);
+        }
+    }
+}
